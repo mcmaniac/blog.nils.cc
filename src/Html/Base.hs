0110e780893d9b39eafcf4062a918a541dff6ecb
Nils Schweinsberg <mail@nils.cc>
2014-08-25 16:36:45 +0000

    Add base Html functions

---
+++ src/Html/Base.hs
@@ -0,0 +1,77 @@
+{-# Language OverloadedStrings #-}
+{-# Language TemplateHaskell #-}
+
+module Html.Base where
+
+import Data.Monoid
+import Control.Monad
+
+-- text package
+import Data.Text
+
+-- lens package
+import Control.Lens
+
+-- blaze-html package
+import Text.Blaze.Html (Html, (!), toValue)
+import qualified Text.Blaze.Html5               as H
+import qualified Text.Blaze.Html5.Attributes    as A
+
+-- our modules
+import Html.Helper
+
+--
+-- HtmlPage definition
+--
+
+type Link = (Text, Text, Text)
+
+data HtmlPage = HtmlPage
+  { _pageTitle   :: Text
+  , _pageScripts :: [FilePath]
+  , _pageStyles  :: [FilePath]
+  , _pageLinks   :: [Link]
+  , _pageBody    :: Html
+  }
+
+makeLenses ''HtmlPage
+
+emptyPage :: HtmlPage
+emptyPage = HtmlPage "" [] [] [] mempty
+
+basePage :: HtmlPage
+basePage = emptyPage
+  & pageTitle   .~ "blog.nils.cc"
+  & pageScripts .~ [ ]
+  & pageStyles  .~ [ "base.css" ]
+  & pageLinks   .~ [ ubuntuFont  ]
+ where
+  ubuntuFont = ("text/css", "http://fonts.googleapis.com/css?family=Ubuntu", "stylesheet")
+
+-- | Render a HtmlPage as Html
+renderPage :: HtmlPage -> Html
+renderPage page = H.docTypeHtml $ do
+
+  H.head $ do
+
+    H.title $ page ^. pageTitle . markup
+
+    -- load javascript
+    forM_ (page ^. pageScripts) $ \s ->
+      H.script ! A.type_ "text/javascript" !
+        A.src (toValue $ "/static/js/" ++ s) $ mempty
+
+    -- load css
+    forM_ (page ^. pageStyles) $ \c ->
+      H.link ! A.type_ "text/css" !
+        A.href (toValue $ "/static/css/" ++ c) ! A.rel "stylesheet"
+
+    -- external links
+    forM_ (page ^. pageLinks) $ \(ty, href, rel) ->
+      H.link ! A.type_ (ty   ^. value)
+             ! A.href  (href ^. value)
+             ! A.rel   (rel  ^. value)
+
+  H.body $ do
+
+    page ^. pageBody
---
+++ src/Html/Helper.hs
@@ -0,0 +1,14 @@
+module Html.Helper where
+
+-- lens package
+import Control.Lens
+
+-- blaze-html package
+import Text.Blaze
+
+-- | Lens for generating `Markup` type
+markup :: ToMarkup a => Getter a Markup
+markup = to toMarkup
+
+value :: ToValue a => Getter a AttributeValue
+value = to toValue
